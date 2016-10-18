e044e17a623ab281b9343b1d437f50127efe1d8f
linmingxiao <lmx@funmi.cn>
2016-10-18 10:24:14 +0800

    Initial Commit

---
+++ ZSImagePickerController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZSImagePickerController
+//
+//  Created by lmx on 16/10/18.
+//  Copyright © 2016年 zhangsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZSImagePickerController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZSImagePickerController
+//
+//  Created by lmx on 16/10/18.
+//  Copyright © 2016年 zhangsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
